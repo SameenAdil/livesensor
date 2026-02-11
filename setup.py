a342c3dae5b1adbe0732db57fb45f2d8c5717f9f
SameenAdil <sameenkainaat1512@gmail.com>
2026-02-11 14:53:12 -0800

    add -e .

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+
+name= "sensor",
+version= "0.0.1",
+author= "Sameen",
+author_email="sameenkainaat1512@gmail.com",
+packages= find_packages(),
+
+)
