57cf713668d9f28da7e1f6970602378c9498740f
SameenAdil <sameenkainaat1512@gmail.com>
2026-02-12 16:18:07 -0800

    after adding -e .

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import find_packages, setup
+from typing import List
 
-
+def get_requirements() -> list[str]:     #output will be in the list(list of string)
+    requirements_list : list[str] = []
+    return requirements_list
 
 setup(
 
@@ -9,5 +12,5 @@ version= "0.0.1",
 author= "Sameen",
 author_email="sameenkainaat1512@gmail.com",
 packages= find_packages(),
-
+install_requires = get_requirements(),  #["pymongo"]
 )
